c1471524bf49700bb3d4b6ef189a3b0815a46a6c
Technote <technote.space@gmail.com>
2021-03-04 12:28:51 +0900

    feat: implement

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-export const add = (num1: number, num2: number): number => num1 + num2;
+import {useCallback} from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type StorageType = Record<string, any>;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type OnChangedHandler = (key: string, value: any) => void;
+
+export type MemoryStorageOptions = {
+  storage: StorageType;
+  onChanged?: OnChangedHandler;
+}
+
+const getStoreValue = <T>(key: string, localStorage: StorageType | undefined, initialValue: T): T => {
+  if (localStorage && key in localStorage) {
+    return localStorage[key];
+  }
+
+  try {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  } catch (error) {
+    console.log(error);
+    return initialValue;
+  }
+};
+
+const useLocalStorage = <T>(key: string, initialValue: T, options?: MemoryStorageOptions): [T, (value: T) => void] => {
+  const storedValue = getStoreValue(key, options?.storage, initialValue);
+  const setValue    = useCallback((value: T): void | never => {
+    options?.onChanged?.(key, value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [storedValue]);
+
+  return [storedValue, setValue];
+};
+
+export default useLocalStorage;
