a45460858cec143241186cb0fd1cc1b39385f8ac
Technote <technote.space@gmail.com>
2022-06-30 00:55:04 +0900

    chore: import order

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-magic-numbers */
-import { describe, expect, it, vi } from 'vitest';
 import { act, renderHook, waitFor } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
 import useLocalStorage from '../src';
 
 vi.stubGlobal('localStorage', (() => {
